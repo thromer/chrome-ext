bf99f5050d3e680307f7f52f8eb2223720724dec
Ted Romer <tromer@gmail.com>
2021-07-05 19:09:20 -0700

    progress

--- chase-amazon-order-id/src/js/contentScript.ts
+++ chase-amazon-order-id/src/js/contentScript.ts
@@ -1,11 +1,21 @@
+// import stringify from 'csv-stringify/lib/sync'
+// const csv = require('csv-stringify/lib/sync')
+// import stringify from 'csv-stringify/lib/sync'
+
 // TODO
 // * search last (30) days
-// * actually scrape everything
-// * put it somewhere useful
-// * encapsulate intermediate steps
+// * apps script to populate order id for me
+// * DONE actually scrape everything
+// * DONE download to csv
+// * DONE encapsulate intermediate steps
 // * casts are scary (' as ')
 // * not-null checks are scary (!)
 // * catch at end of promises
+// * maybe wait for one of 'You've reached the end of your account activity.'
+// * do it when requested not eagerly!
+// * do it in hiding (doubtful!)
+
+import * as save_file from './save_file';
 
 function myLog(s: string) {
   console.log('THROMER ' + s)
@@ -14,6 +24,15 @@ function myLog(s: string) {
 function stringifyMap(m: Map<string, string>) {
   return Array.from(m.entries()).map(e => e[0] + ' => ' + e[1]).join(", ")
   // return JSON.stringify(Object.fromEntries(m.entries()))
+}
+
+const KEYS = ['sale', 'transaction date', 'posted date', 'merchant info', 'description', 'also known as', 'method', 'card number', 'category', 'amazon order number']
+
+function toCsv(answer: Map<string, string>[]) {
+  const table = [KEYS].concat(
+    answer.map(m => KEYS.map(k => m.get(k) || '')))
+  // return csv.stringify(table)
+  return table.map(row => row.map(e => '"' + e + '"').join(',')).join('\n')
 }
 
 async function analyzeElement(elementString: string): Promise<Map<string, string>> {
@@ -27,11 +46,53 @@ async function analyzeElement(elementString: string): Promise<Map<string, string
   return answer
 }
 
-function main() {
-  mainHardcodedArray()
+async function analyzeElementElement(element: HTMLElement, label: string): Promise<Map<string, string>> {
+  element.click()
+  await waitForTransactionDetails(label)
+  const answer = transactionDetails(label + ' again')
+  const closeElement: HTMLElement = await waitForElement('#flyoutClose')
+  closeElement.click()
+  // myLog('not fully returned answer ' + stringifyMap(answer))
+  return answer
 }
 
-async function mainWithHardcodedArrayPartial() : Promise<Map<string, string>[]>{
+function main() {
+  mainWithDynamicArray()
+}
+
+async function transactionList() {
+  const table = await waitForElement('#activityTablesingleOVDAccountActivity')
+  const clickableTransactions = table.querySelectorAll('.etd-action-icon')
+}
+
+function mainWithDynamicArray() {
+
+  mainWithDynamicArrayPartial()
+    .then(function(x) {
+      const csv = toCsv(x)
+      myLog(csv)
+      myLog(x.map(e => stringifyMap(e)).join("; "))
+      save_file.save(csv, 'chase_order_numbers.csv')
+    })
+  
+}
+
+async function mainWithDynamicArrayPartial() {
+  // TODO will this run too soon?
+  const footer = await waitForElement('.transaction-footer')
+  myLog('footer text ' + footer.textContent)
+  const table = await waitForElement('#activityTablesingleOVDAccountActivity')
+  const clickableTransactions = table.querySelectorAll('.etd-action-icon')
+  let answer: Map<string, string>[] = [];
+  let i = 0 
+  for (const e of clickableTransactions) {
+    answer.push(await analyzeElementElement(e as HTMLElement, (i+1).toString()))
+    i++
+  }
+  return answer
+}
+
+async function mainWithHardcodedArrayPartial() : Promise<Map<string, string>[]> {
   const elementStrings = ['#transactionDetailIcon0', '#transactionDetailIcon1', '#transactionDetailIcon2']
   var answer: Map<string, string>[] = [];
   for (const e of elementStrings) {
@@ -41,8 +102,14 @@ async function mainWithHardcodedArrayPartial() : Promise<Map<string, string>[]>{
 }
 
 function mainHardcodedArray() {
+
   mainWithHardcodedArrayPartial()
-    .then(function(x) {myLog(x.map(e => stringifyMap(e)).join("; "))})
+    .then(function(x) {
+      const csv = toCsv(x)
+      myLog(csv)
+      myLog(x.map(e => stringifyMap(e)).join("; "))
+      save_file.save(csv, 'chase_order_numbers.csv')
+    })
 }
 
 function waitForTransactionDetails(transactionId: string) {
@@ -59,11 +126,16 @@ function waitForTransactionDetails(transactionId: string) {
 	e => e.textContent!.toLowerCase().trim())  // TypeScript
       const dds = Array.from(document.querySelectorAll('dd')).map(
 	e => e.textContent || (e.querySelector('mds-link')! as HTMLElement).getAttribute('text'))
-      return dts.includes('amazon order number') &&
-	dds.length >= dts.length && m.has('amazon order number')
+      const satisfiedOne = dts.includes('total amazon rewards points') &&
+	dds.length >= dts.length && m.has('total amazon rewards points')
+      const satisfiedTwo = dts.includes('merchant info') && m.get('merchant info') == 'REDEMPTION CREDIT'
+      return satisfiedOne || satisfiedTwo
     },
+      
+      // },
     10000,
-    'waiting for amazon order number'
+    //    'waiting for amazon order number'
+    'waiting for total amazon rewards points'
   ).then(() => transactionId)
   // TODO are we supposed to catch too
 }
@@ -78,7 +150,7 @@ function transactionDetails(transactionId: string) : Map<string, string> {
   myLog('dds.length= ' + dts.length)
   const m = new Map<string, string>()
   for (let i = 0; i < dts.length; i++) {
-    m.set(dts[i]!.trim().toLowerCase(), dds[i]!)
+    m.set(dts[i]!.trim().toLowerCase(), dds[i]!.trim())
   }
 
   // My way works in browser but not in extension?  This is from
---
+++ chase-amazon-order-id/src/js/save_file.ts
@@ -0,0 +1,15 @@
+/* Copyright(c) 2019 Philip Mulcahy. */
+
+// From azad
+
+export function save(file_content_string: string, filename: string): void {
+    const blob = new Blob([file_content_string], { type: 'text/csv;charset=utf-8;' });
+    const link = document.createElement("a");
+    const url = URL.createObjectURL(blob);
+    link.setAttribute("href", url);
+    link.setAttribute("download", filename);
+    link.style.visibility = 'hidden';
+    document.body.appendChild(link);
+    link.click();
+    document.body.removeChild(link);
+}
