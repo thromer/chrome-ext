2e67ad1a7f9873be65d982a20630d008b210cbaf
Ted Romer <tromer@gmail.com>
2021-07-06 20:09:26 -0700

    sheets api from background page

---
+++ hello-ts/src/js/oauth.ts
@@ -0,0 +1,28 @@
+window.onload = function() {
+  document.querySelector('button')!.addEventListener('click', function() {
+    chrome.identity.getAuthToken({interactive: true}, function(token) {
+      let init = {
+        method: 'GET',
+        async: true,
+        headers: {
+          Authorization: 'Bearer ' + token,
+          'Content-Type': 'application/json'
+        },
+        'contentType': 'json'
+      };
+      fetch(
+	'https://content-sheets.googleapis.com/v4/spreadsheets/1UQQgW3kBfxNBB50q1pg4Hn2c6DvwoKVUF_9GelZ1k1Q?key=AIzaSyDAK21zJnikmqXn5GXurH5O2neIb2GCerg',
+          init)
+          .then((response) => response.json())
+          .then(function(data) {
+	    alert(JSON.stringify(data))
+            console.log(JSON.stringify(data))  // goes into a black hole :(
+          })
+	.catch(function(e) {
+	  console.log(e)  // goes into a black hole :(
+	  alert(e)
+	})
+    });
+  });
+};
+
